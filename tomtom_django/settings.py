7a421ee2dc28ea3e6d0d6057ec40e6c10fca1e0a
mgrundmo <mgrundmo@gmail.com>
2025-11-06 11:55:23 +0100

    udate to admin.py to see user table for admin

--- tomtom_django/settings.py
+++ tomtom_django/settings.py
@@ -124,11 +124,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'productionstatic'
-
 STATIC_URL = 'static/'
 
 STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 MEDIA_URL = 'media/'
 
